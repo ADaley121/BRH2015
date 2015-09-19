89449262d72e07729841ff8a8afc092d172e4b47
John Hughes <JohnH@Johns-Mac-mini.local>
2015-09-19 07:13:44 -0700

    began event table view controller

--- BRH2015/EventTableViewController.swift
+++ BRH2015/EventTableViewController.swift
@@ -7,15 +7,50 @@
 //
 
 import UIKit
+import EventKit
 
-class EventTableViewController: UIViewController {
+class EventTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var events: [EKEvent] = []
     
     @IBOutlet weak var eventTableView:UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let eventStore = EKEventStore()
+        
+        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
+        case .Authorized:
+            var calendars = eventStore.calendarsForEntityType(EKEntityTypeEvent)
+            var filteredCalendars = calendars.filter { c in
+                if let c = c as? EKCalendar {
+                    return c.title == "Uber"
+                } else {
+                    return false
+                }
+            } as! [EKCalendar]
+            if filteredCalendars.isEmpty {
+                
+            } else {
+                
+            }
+        case .Denied:
+            println("Access Denied")
+        case .NotDetermined:
+            println("Status not determined")
+        default:
+            println("default case")
+        }
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
