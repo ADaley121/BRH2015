ee45e58257d6696562d63e4427baf25692f2fb12
John Hughes <JohnH@Johns-Mac-mini.local>
2015-09-19 21:36:34 -0700

    Added checks and alerts in new event vc

--- BRH2015/NewEventViewController.swift
+++ BRH2015/NewEventViewController.swift
@@ -41,6 +41,11 @@ class NewEventViewController: UIViewController {
         endDatePicker.addTarget(self, action: Selector("handleEndDatePicker:"), forControlEvents: UIControlEvents.ValueChanged)
         endTimeField.inputView = endDatePicker
         
+        startDatePicker.setDate(NSDate(timeIntervalSinceNow: 0), animated: true)
+        startTimeField.text = startDateFormat.stringFromDate(NSDate(timeIntervalSinceNow: 0))
+        endDatePicker.setDate(NSDate(timeIntervalSinceNow: 3600), animated: true)
+        endTimeField.text = endDateFormat.stringFromDate(NSDate(timeIntervalSinceNow: 3600))
+        
         if let e = event {
             self.titleField.text = e.title
             self.locationField.text = e.location
@@ -53,73 +58,94 @@ class NewEventViewController: UIViewController {
     
     func handleStartDatePicker(sender: UIDatePicker) {
         startTimeField.text = startDateFormat.stringFromDate(sender.date)
+        if sender.date.compare(endDatePicker.date) == NSComparisonResult.OrderedDescending {
+            endTimeField.textColor = UIColor.redColor()
+        } else {
+            endTimeField.textColor = UIColor.blackColor()
+        }
     }
     
     func handleEndDatePicker(sender: UIDatePicker) {
         endTimeField.text = endDateFormat.stringFromDate(sender.date)
+        if sender.date.compare(startDatePicker.date) == NSComparisonResult.OrderedAscending {
+            endTimeField.textColor = UIColor.redColor()
+        } else {
+            endTimeField.textColor = UIColor.blackColor()
+        }
     }
     
     @IBAction func addEvent(sender: UIButton) {
-        let eventStore = EKEventStore()
-        
-        switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
-        case .Authorized:
-            var calendars = eventStore.calendarsForEntityType(EKEntityTypeEvent)
-            var filteredCalendars = calendars.filter { c in
-                if let c = c as? EKCalendar {
-                    return c.title == "Uber"
-                } else {
-                    return false
+        if titleField.text == "" {
+            let alert = UIAlertController(title: "Could Not Save", message: "You must have an event title.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else if startDatePicker.date.compare(endDatePicker.date) == NSComparisonResult.OrderedDescending {
+            let alert = UIAlertController(title: "Could Not Save", message: "Your event must end after it starts.", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        } else {
+            let eventStore = EKEventStore()
+            
+            switch EKEventStore.authorizationStatusForEntityType(EKEntityTypeEvent) {
+            case .Authorized:
+                var calendars = eventStore.calendarsForEntityType(EKEntityTypeEvent)
+                var filteredCalendars = calendars.filter { c in
+                    if let c = c as? EKCalendar {
+                        return c.title == "Uber"
+                    } else {
+                        return false
+                    }
+                    } as! [EKCalendar]
+                if filteredCalendars.isEmpty {
+                    let newCalendar = EKCalendar(forEntityType: EKEntityTypeEvent, eventStore: eventStore)
+                    newCalendar.title = "Uber"
+                    
+                    let sourcesInEventStore = eventStore.sources() as! [EKSource]
+                    
+                    newCalendar.source = sourcesInEventStore.filter {(source: EKSource) -> Bool in
+                        source.sourceType.value == EKSourceTypeLocal.value}.first
+                    
+                    filteredCalendars.append(newCalendar)
+                    var error: NSError? = nil
+                    let calendarWasSaved = eventStore.saveCalendar(newCalendar, commit: true, error: &error)
+                    
+                    if !calendarWasSaved {
+                        let alert = UIAlertController(title: "Calendar could not save", message: error?.localizedDescription, preferredStyle: .Alert)
+                        let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                        alert.addAction(OKAction)
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    } else {
+                        NSUserDefaults().setObject(newCalendar.calendarIdentifier, forKey: "EventTrackerPrimaryCalendar")
+                    }
                 }
-            } as! [EKCalendar]
-            if filteredCalendars.isEmpty {
-                let newCalendar = EKCalendar(forEntityType: EKEntityTypeEvent, eventStore: eventStore)
-                newCalendar.title = "Uber"
+                var event = EKEvent(eventStore: eventStore)
+                event.calendar = filteredCalendars[0]
                 
-                let sourcesInEventStore = eventStore.sources() as! [EKSource]
+                event.title = titleField.text
+                event.location = locationField.text
+                event.startDate = startDatePicker.date
+                event.endDate = endDatePicker.date
+                event.notes = notesField.text
                 
-                newCalendar.source = sourcesInEventStore.filter {(source: EKSource) -> Bool in
-                    source.sourceType.value == EKSourceTypeLocal.value}.first
-              
-                filteredCalendars.append(newCalendar)
-                var error: NSError? = nil
-                let calendarWasSaved = eventStore.saveCalendar(newCalendar, commit: true, error: &error)
+                // Save Event in Calendar
+                var error: NSError?
+                let result = eventStore.saveEvent(event, span: EKSpanThisEvent, error: &error)
                 
-                if !calendarWasSaved {
-                    let alert = UIAlertController(title: "Calendar could not save", message: error?.localizedDescription, preferredStyle: .Alert)
-                    let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-                    alert.addAction(OKAction)
-                    self.presentViewController(alert, animated: true, completion: nil)
-                } else {
-                    NSUserDefaults().setObject(newCalendar.calendarIdentifier, forKey: "EventTrackerPrimaryCalendar")
+                if result == false {
+                    if let theError = error {
+                        println("An error occured \(theError)")
+                    }
                 }
+                
+                
+            case .Denied:
+                println("Access denied")
+            case .NotDetermined:
+                println("Status not determined")
+            default:
+                println("Case Default")
             }
-            var event = EKEvent(eventStore: eventStore)
-            event.calendar = filteredCalendars[0]
-            
-            event.title = titleField.text
-            event.location = locationField.text
-            event.startDate = startDatePicker.date
-            event.endDate = endDatePicker.date
-            event.notes = notesField.text
-            
-            // Save Event in Calendar
-            var error: NSError?
-            let result = eventStore.saveEvent(event, span: EKSpanThisEvent, error: &error)
-            
-            if result == false {
-                if let theError = error {
-                    println("An error occured \(theError)")
-                }
-            }
-            
-            
-        case .Denied:
-            println("Access denied")
-        case .NotDetermined:
-            println("Status not determined")
-        default:
-            println("Case Default")
+
         }
     }
     
