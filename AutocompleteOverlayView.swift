b7a517c5aa30f60339ed6724e3417a3c99de687f
ADaley121 <ajd93@cornell.edu>
2015-09-19 23:56:54 -0400

    autocomplete working on request vc

---
+++ AutocompleteOverlayView.swift
@@ -0,0 +1,21 @@
+//
+//  AutocompleteOverlayView.swift
+//  
+//
+//  Created by Andrew Daley on 9/19/15.
+//
+//
+
+import UIKit
+
+class AutocompleteOverlayView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
